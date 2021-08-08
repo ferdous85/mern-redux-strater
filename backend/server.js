09f072b03e1cd2c38dce621681e6021ab800f020
ferdous85 <ferdous85@gmail.com>
2021-08-08 19:28:11 +0600

    notes api added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const notes = require('./data/notes')
 const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
+const noteRoutes = require('./routes/noteRoutes')
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
 
 const app = express()
@@ -12,6 +13,8 @@ connectDB()
 app.use(express.json())
 
 app.use('/api/users', userRoutes)
+app.use('/api/notes', noteRoutes)
+
 app.use(notFound)
 app.use(errorHandler)
 
