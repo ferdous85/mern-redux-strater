87d13ee46e05feb6c3a284844d4c8db8e89d44ee
ferdous85 <ferdous85@gmail.com>
2021-08-08 22:21:40 +0600

    backend jobs

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const { getNotes } = require('../controllers/noteControllers')
 
 const router = express.Router()
 
-router.route('/').post((req, res)=>{
-  res.json({msg:'Api is running'})
-})
+router.route('/').get(getNotes)
+
+//router.route('/create').post()
+//router.route('/:id').get().put().delete()
 
 module.exports = router
